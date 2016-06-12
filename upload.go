2014c6c6e0ca0af1cf2f7bb82e48b3d7322c71fa
Eliot Whalan <ewhal@pantsu.cat>
2016-06-12 15:05:32 +1000

    Add base functions

---
+++ upload.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/dchest/uniuri"
+	"io"
+	"net/http"
+	"os"
+)
+
+const (
+	LENGTH = 6
+)
+
+func startup() {
+
+}
+
+func DB() {
+
+}
+
+func exists() {
+
+}
+
+func getHash() {
+
+}
+
+func uploadHandler() {
+
+}
+
+func main() {
+
+}
