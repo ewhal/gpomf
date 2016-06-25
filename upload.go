22fda8ebbce0d1968104078e4bc92f5db9eae4bc
Eliot Whalan <ewhal@pantsu.cat>
2016-06-25 13:43:06 +1000

    Add file name generation

--- upload.go
+++ upload.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"crypto/sha1"
+
 	//	"encoding/json"
 	"fmt"
 	"io"
@@ -18,36 +18,14 @@ const (
 	DIRECTORY = "/tmp/"
 )
 
-func startup() {
+//func getHash(file io.Reader) string {
+//	h := sha1.New()
+//	return string(h.Sum(nil))
 
-}
-
-func DB() {
-
-}
-
-func exists(location string) bool {
-	if _, err := os.Stat(DIRECTORY + location); err != nil {
-		if os.IsNotExist(err) {
-			return false
-		}
-	}
-	return true
-
-}
-
-func getHash(file []byte) string {
-	h := sha1.New()
-	io.WriteString(h, string(file))
-	return string(h.Sum(nil))
-
-}
+//}
 
 func generateName() string {
 	name := uniuri.NewLen(LENGTH)
-	if exists(name) == true {
-		generateName()
-	}
 	return name
 }
 func check(err error) {
@@ -70,18 +48,17 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		//copy each part to destination.
 		for {
 			part, err := reader.NextPart()
 			if err == io.EOF {
 				break
 			}
 
-			//if part.FileName() is empty, skip this iteration.
 			if part.FileName() == "" {
 				continue
 			}
-			dst, err := os.Create(DIRECTORY + part.FileName())
+			s := generateName()
+			dst, err := os.Create(DIRECTORY + s + ".txt")
 			defer dst.Close()
 
 			if err != nil {
@@ -93,7 +70,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			io.WriteString(w, part.FileName()+"\n")
+			io.WriteString(w, s+"\n")
 		}
 	}
 
