9386dc4fd2b163c94686f6563d069530724d821f
Jeff <jeff@lokinet.io>
2022-05-18 10:16:23 -0400

    squelch multiple calls to writing response

--- pomf.go
+++ pomf.go
@@ -61,6 +61,7 @@ type Response struct {
 	ErrorCode   int      `json:"errorcode,omitempty"`
 	Description string   `json:"description,omitempty"`
 	Files       []Result `json:"files,omitempty"`
+	sent        bool
 }
 
 // generateName returns a random string that isn't in the database
@@ -90,13 +91,20 @@ func generateName() (string, error) {
 
 // respond outputs Response struct in user specified formats
 // supported formats are xml, json, text and html
-func respond(w http.ResponseWriter, output string, resp Response) {
+func respond(w http.ResponseWriter, output string, resp *Response) {
+	resp.sent = true
 	if resp.ErrorCode != 0 {
 		resp.Files = []Result{}
 		resp.Success = false
 		w.WriteHeader(resp.ErrorCode)
+		io.WriteString(w, resp.Description)
+		return
 	} else {
 		resp.Success = true
+	}
+	if resp.sent {
+		// bail we already sent shit back
+		return
 	}
 
 	switch output {
@@ -167,7 +175,7 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	output := r.FormValue("output")
 	reader, err := r.MultipartReader()
-	resp := Response{Files: []Result{}}
+	resp := &Response{Files: []Result{}}
 	if err != nil {
 		resp.ErrorCode = http.StatusInternalServerError
 		resp.Description = err.Error()
